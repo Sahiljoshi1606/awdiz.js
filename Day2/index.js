025dc681609bc15c57cff31b0db01cff5219d5b5
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-06-25 22:50:12 +0530

    update

---
+++ Day2/index.js
@@ -0,0 +1,26 @@
+// javascript day 2
+// =
+// ==
+// ===
+console.log(1 == "1"); // true
+console.log(1 === "1"); // false
+var a = 1; //assign a=1
+console.log(a == 1); // true
+console.log(a === 1); // true
+console.log(a === "1"); // false
+console.log(a == 2); // false
+
+// grater than and less than
+
+console.log(1 > 1); // false
+console.log(1 >= 1); // true
+console.log(1 < 10); // true
+console.log(1 <= 10); // true
+
+// AND OR 
+
+console.log(10 > 5 && 5 < 10); // true
+console.log(10 > 20 && 10 > 5); // false
+console.log(10 > 5 || 5 < 10); // true
+console.log(10 > 20 || 10 > 5); // true
+console.log(5 > 6 || 5 > 8); // false
