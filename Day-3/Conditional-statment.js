10fba30c10c88ebba6874e6a2d16c87c034c94c9
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-06-29 17:43:01 +0530

    Conditional statment

---
+++ Day-3/Conditional-statment.js
@@ -0,0 +1,148 @@
+// Q1)
+// let num = 4;
+// if (num > 0) {
+//   console.log("Positive number");
+// }
+//   else if (num < 0)
+//     {
+//     console.log("Negative number");
+//   }
+//   else {
+//     console.log("Zero");
+//   }
+
+// Q2)
+// let num = 111;
+// if(num % 2 ==0){
+//     console.log("Even number");
+// }
+// else if ( num % 2 == 1){
+//     console.log("Odd number");
+// }
+
+// Q3)
+// let num= 210;
+// if( num < 13){
+//     console.log("child");
+// }
+// else if ( num >= 13 && num <= 20){
+//     console.log("teenager");
+// }
+// else if ( num >=21 && num <= 60){
+//     console.log("adult");
+// }
+// else if ( num > 60){
+//     console.log("senior ");
+// }
+
+// Q4)
+
+// let grade = 9;
+// if( grade >= 90) {
+//     console.log("A");
+// }
+// else if ( grade >=80 && grade <= 89) {
+//     console.log("B");
+// }
+// else if ( grade >=70 && grade  <=79){
+//     console.log("C");
+// }
+// else if ( grade >=60 && grade <=69){
+//     console.log("D");
+// }
+// else if ( grade < 60){
+//     console.log("F");
+// }
+
+// Q5)
+
+// let day = 2;
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//     case 2:
+//     console.log("Tuesday");
+//     break;
+//     case 3:
+//     console.log("Wednesday");
+//     break;
+//     case 4:
+//     console.log("Thursday");
+//     break;
+//     case 5:
+//     console.log("Friday");
+//     break;
+//     case 6:
+//     console.log("Saturday");
+//     break;
+//     case 7:
+//     console.log("Sunday");
+
+// }
+
+// Q6)
+
+// let char = "y";
+// switch (char) {
+//   case "a":
+//   case "e":
+//   case "i":
+//   case "o":
+//   case "u":
+//     console.log("Vowel");
+//     break;
+//   default:
+//     console.log("Consonant");
+// }
+
+// Q7)
+// let year = 2028;
+// if(year % 4 === 0 ) {
+//     console.log("Leap year");
+// }
+// else {
+//     console.log("Not a leap year");
+// }
+
+// Q8)
+// let firstnumber = 10;
+// let secondnumber = 20;
+// let operator = "-";
+// switch (operator) {
+//   case "+":
+//     console.log(firstnumber + secondnumber);
+//     break;
+//   case "-":
+//     console.log(firstnumber - secondnumber);
+//     break;
+//   case "*":
+//     console.log(firstnumber * secondnumber);
+//     break;
+//   case "/":
+//     console.log(firstnumber / secondnumber);
+//     break;
+//     default:
+//     console.log("Invalid operator");
+//     break;
+// }
+
+// Q9)
+// let age = 18;
+// if( age >= 18){
+//     console.log ("Eligible to vote");
+// }
+// else{
+//     console.log("Not eligible to vote");
+// }
+
+// Q10)
+// let a = 10;
+// let b = 10;
+// let c = 10;
+// if ( a + b >c && a + c > b && b + c > a) {
+//     console.log("Valid triangle");
+// }
+// else {
+//     console.log("Invalid triangle");
+// }
