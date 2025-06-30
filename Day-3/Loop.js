2580df32b932ff5b16158a28b7ed9d030b7aee26
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-06-30 08:40:56 +0530

    update

---
+++ Day-3/Loop.js
@@ -0,0 +1,52 @@
+// Q1)
+
+// let i=1;
+// for (i=1 ; i<=50; i++){
+//     if (i %2 == 0){
+//         console.log(i + " is even");
+//     }
+// }
+
+// Q2)
+
+// let i = 10;
+// while (i >=0){
+//     console.log(i);
+//     i--;
+// }
+
+// Q3)
+
+// let number = 7;
+// for (let i = 1; i <= 10; i++) {
+//     console.log (number * i);
+// }
+
+
+//Q4)
+
+// for(i = 1; i<=100; i++ ){
+//     if( i % 2 == 1){
+//         console.log(i);
+//     }
+// }
+
+
+// Q5)
+// let count = 0;
+// for (i = 1 ; i <=100 ; i++){
+//     if (i % 3 == 0 && i % 5 ==0){
+//        count++;
+//     }
+// }
+// console.log(count);
+
+// Q6)
+  let num = 5;
+  let i = 1;
+  let factorial = 1;
+  while (i <= num) {
+    factorial = factorial * i;
+    i++;
+  }
+  console.log(factorial)
