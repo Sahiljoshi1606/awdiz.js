cff262afc53ebb15ef79b8320bda94d21daa3481
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-07-02 23:15:52 +0530

    update

---
+++ Day-5/Array.js
@@ -0,0 +1,23 @@
+let array = [1,2,3,4,5,6,7,8,9,10];
+let count = 0;
+for ( i =0 ; i< array.length; i++) {
+    if (array[i] % 2 === 0) {
+        count++;
+    }
+}
+console.log(count)
+Output: 5
+
+
+
+
+let array = [2, 3, 2, 4, 5, 6, 12, 123, 14, 43];
+let age = 14;
+for (i = 9; i >= 0; i--) {
+  if (array[i] == age) {
+    console.log(i);
+  }
+}
+
+
+
