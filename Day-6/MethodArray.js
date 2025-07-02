4d364b9f860ea1a5d6a4cb6b08f1ec34d00abe54
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-07-02 23:15:14 +0530

    update

---
+++ Day-6/MethodArray.js
@@ -0,0 +1,31 @@
+// Map()
+
+// let array = [2,3,4];
+// let MultiplyByFive = array.map((array)=> array * 5);
+// console.log(MultiplyByFive);
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let add = num.map((numbers)=> numbers + 10);
+// console.log(add);
+
+
+// Filter()
+
+
+// let num = 2;
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let filter = array.filter((Remove)=> Remove == num);
+// console.log(filter);
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let filter = array.filter ((even)=> even % 2 == 0);
+// console.log(filter);
+
+
+
+// foreach
+
+let student = [ "om","sahil", "jitesh","ajay"] ;
+let FullName = student.map((Surname)=> Surname + "Joshi" )
+console.log(FullName);
+    
