492401c0069af88430bded9d44b9176b9fd0d538
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-07-04 10:55:22 +0530

    practice

---
+++ Day-7/practice.js
@@ -0,0 +1,104 @@
+// let count = 0;
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 == 0) {
+//     count++;
+//   }
+// }
+// console.log(count);
+
+// for ( let i= 0 ; i <= 15; i++){
+//     if ( i % 2 == 0){
+//         console.log(i + " is even ");
+//     }
+//     else if ( i % 2 == 1){
+//         console.log (i + " is odd ");
+//     }
+// }
+// let sum = 0;
+// for ( let i=1 ; i<=100 ; i++){
+//  sum +=i;
+// }
+// console.log(sum);
+
+// let count = 0;
+// for (let i = 1 ; i<=50; i++ ){
+//     if ( i % 2 == 0){
+//         count++;
+//     }
+// }
+// console.log(count);
+
+// const array = [5 , 15, 10];
+// let mul = array.map ((sum)=> sum*2);
+// console.log(mul);
+
+// const orignal = "sahil";
+// let reverse = "";
+//  for ( let i = orignal.length -1 ; i >= 0; i--){
+//     reverse += orignal[i];
+//  }
+//  console.log(reverse);
+
+// let i = 1;
+// while ( i <= 1000){
+//     console.log(i);
+//     i *= 2;
+// }
+// console.log(i);
+
+// let arr = ["Alice","Bob","Charlie"];
+// let apply = arr.map((name)=>" Hello, " + name  );
+// console.log(apply);
+
+// for ( let i =1 ; i <=100 ; i++ ){
+//     if ( i % 3 ==0){
+//         console.log ("Fizz");
+//     }
+//     else if ( i% 5 == 0){
+//         console.log ("Buzz");
+//     }
+//     else if ( i % 3 == 0 && i % 5 == 0){
+//         console.log ("FizzBuzz");
+//     }
+//     else {
+//         console.log (i);
+//     }
+// }
+
+// let arr = [1,2,3];
+// arr.push(4);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// arr.shift();
+// console.log(arr);
+// arr.unshift(0);
+// console.log
+
+// let arr1 =[1,2,3];
+// let arr2 = [4,5,6];
+//  let arr3 = arr1.concat(arr2);
+//  console.log(arr3);
+
+// let arr = ["jan", "feb", "mar"];
+// arr.splice(1, 0, "april");
+// console.log(arr); // Output: ["jan", "april", "mar"]
+// arr.push("may");
+// console.log(arr); // Output: ["jan", "april", "mar", "may"]
+// arr.splice( 1, 0, "feb");
+// console.log(arr); // Output: ["jan", "feb", "april", "mar", "may"]
+
+
+
+
+// let i = 1;
+// while (i <= 100) {
+//   if (i >= 50) {
+ 
+ 
+//   break;
+//   }
+//   i++;
+//   console.log(i);
+// }
+
