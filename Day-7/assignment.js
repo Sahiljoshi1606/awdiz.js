20a3e16208297af0eac753523ba0fb82234927b7
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-07-08 22:57:14 +0530

    update

--- Day-7/assignment.js
+++ Day-7/assignment.js
@@ -123,16 +123,74 @@
 //  })
 //  console.log(num);
 
-
-// 10) Filter numbers divisible by 3 and 5. 
-// Input: [10, 15, 30, 20, 45] 
-// Output: [15, 30, 45] 
-
+// 10) Filter numbers divisible by 3 and 5.
+// Input: [10, 15, 30, 20, 45]
+// Output: [15, 30, 45]
 
 // let arr = [10, 15, 30, 20, 45] ;
 // number = arr.filter(function(num){
 //   return num % 3 == 0 && num % 5 == 0;
 // })
 // console.log(number)
+
+// 11) Fibonacci sequence up to 15 terms.
+// Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+
+// let Fibonacci = [0,1]
+// for ( i =2 ;  i < 15 ; i++){
+//     Fibonacci[i] = Fibonacci[i - 1] + Fibonacci[i-2];
+
+// }
+//    console.log(Fibonacci)
+
+// 12)  Count occurrences of a value in array.
+// Input: [1, 2, 1, 3, 1, 4], Element: 1
+// Output: 3
+
+// 13)Sum of even numbers using reduce.
+// Input: [1, 2, 3, 4, 5, 6]
+// Output: 12
+
+// let arr = [1, 2, 3, 4, 5, 6]
+// let elements = arr.reduce((acc , curr)=>{
+//     return curr % 2  == 0 ? acc + curr : acc
+// } , 0)
+// console.log(elements)
+
+//  14)Check if string is palindrome.
+// Input: "madam"
+// Output: true
+
+// let string = "madam" ;
+// let palindrome = string == string.split("").reverse().join("");
+// console.log(palindrome)
+
+//  15)Return grade based on marks using switch.
+// Input: Marks: 85
+// Output: B
+// let marks = 85;
+// let gradeLevel = Math.floor(marks / 10);
+// let grade;
+
+// switch (gradeLevel) {
+//   case 10:
+//   case 9:
+//     grade = "A";
+//     break;
+//   case 8:
+//     grade = "B";
+//     break;
+//   case 7:
+//     grade = "C";
+//     break;
+//   case 6:
+//     grade = "D";
+//     break;
+//   default:
+//     grade = "fail";
+// }
+
+// console.log(grade);
+
 
 
