525ef25977d65ffd81f086b9fab8b8e5d786f1cb
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-07-11 19:49:06 +0530

    update

---
+++ Day-7/home.js
@@ -0,0 +1,40 @@
+// let string =["sahil" , "om"];
+// let reverse = [];
+// for( i = 0 ; i < string.length ; i++){
+//    let word = string[i]
+//    let reversestring = "";
+//     for ( let j = word.length -1  ; j >= 0; j-- ){
+//     reversestring += word[j] ; 
+//     }
+//     reverse.push(reversestring)
+// }
+// console.log (reverse)
+
+
+
+// let arr = ["hello", "world"];
+// let reversearr = [];
+// for (let i = 0; i < arr.length; i++) {
+//   let word = arr[i];
+//   console.log(word);
+//   let reverse = "";
+//   for (let j = word.length - 1; j >= 0; j--) {
+//     reverse += word[j];
+//   }
+//   reversearr.push(reverse);
+// }
+// console.log(reversearr);
+
+// let string = "apple";
+// let frequency = {};
+// for (let i = 0; i < string.length; i++) {
+//   let char = string[i];
+//   console.log(frequency);
+//   if (frequency[char]) {
+//     frequency[char]++;
+//      console.log(frequency)
+//   } else {
+//     frequency[char] = 1;
+//   }
+// }
+
