75c7b0088adf7cfa6e833b67dc9c5e5af2a9f702
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-07-23 00:08:05 +0530

    update

--- Day - 15/index.js
+++ Day - 15/index.js
@@ -1,41 +1,41 @@
 // 2D arra
 
-// var num = [
-//   [1, 2, 3],
-//   [4, 5, 6],
-//   [7, 8, 9],
-// ,
-// ];
-// let left = 0 ;
-// let right = num[0].length - 1;
-// let top = 0 ;
-// let bottom = num.length - 1 ;
+var num = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+,
+];
+let left = 0 ;
+let right = num[0].length - 1;
+let top = 0 ;
+let bottom = num.length - 1 ;
 
-// while(left <= right && top <= bottom){
-//     for (let i = left ; i <= right ; i++){
-//         console.log(num [top][i])
+while(left <= right && top <= bottom){
+    for (let i = left ; i <= right ; i++){
+        console.log(num [top][i])
 
-//     }
-//     top++;
-// for (let i = top ; i <= bottom ; i++){
-//     console.log(num[i][right])
-// }
-// right--
+    }
+    top++;
+for (let i = top ; i <= bottom ; i++){
+    console.log(num[i][right])
+}
+right--
 
-// if(top <= bottom){for (let i = right ; i >= left ; i--){
-//     console.log(num[bottom][i])
-// }
-// bottom--;
-// }
+if(top <= bottom){for (let i = right ; i >= left ; i--){
+    console.log(num[bottom][i])
+}
+bottom--;
+}
 
-// if(left <= right){
-//     for(let i = bottom ; i >= top ; i-- ){
-//     console.log(num[i][left])
-// }
-// left++
-// }
+if(left <= right){
+    for(let i = bottom ; i >= top ; i-- ){
+    console.log(num[i][left])
+}
+left++
+}
 
-// }
+ }
 
 
 
